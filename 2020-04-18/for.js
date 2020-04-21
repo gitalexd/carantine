29f4ddb4d9f4e23ad885d630d490e366bcb8ac34
Sasha <89680590522@mail.ru>
2020-04-21 20:49:40 +0300

    init

---
+++ 2020-04-18/for.js
@@ -0,0 +1,9 @@
+// Создать массив от одного до 10 <br>
+// вывести этот массив на экран так чтобы каждый элемент был умножен на 2
+
+const a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let list = document.getElementById('list');
+
+for (let i = 0; i < a.length; i++) {
+    console.log(parseInt(a[i]))
+}
